635d395e2db52788d16c96c0f27bc1ded61641ba
ngoctien308 <ngoctien30804@gmail.com>
2024-12-04 10:18:19 +0700

    chapter 7

--- app/query/route.ts
+++ app/query/route.ts
@@ -6,9 +6,9 @@ async function listInvoices() {
 	const data = await client.sql`
     SELECT invoices.amount, customers.name
     FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
-  `;
+    `;
+    // JOIN customers ON invoices.customer_id = customers.id
+    // WHERE invoices.amount = 666;
 
 	return data.rows;
 }
